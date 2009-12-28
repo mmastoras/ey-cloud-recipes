5f24564ebcc8168cb9c1d2a6462c6807c3e522a0
mmastoras <mmastoras@proaptitude.com>
2009-12-27 17:07:21 -0800

    enable thinking_sphinx recipes

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -8,16 +8,16 @@ end
 # require_recipe "couchdb"
 
 # uncomment to turn use the MBARI ruby patches for decreased memory usage and better thread/continuationi performance
-# require_recipe "mbari-ruby"
+#require_recipe "mbari-ruby"
 
 # uncomment to turn on thinking sphinx 
-# require_recipe "thinking_sphinx"
+require_recipe "thinking_sphinx"
 
 # uncomment to turn on ultrasphinx 
 # require_recipe "ultrasphinx"
 
 #uncomment to turn on memcached
-# require_recipe "memcached"
+require_recipe "memcached"
 
 #uncomment to run the authorized_keys recipe
 #require_recipe "authorized_keys"
