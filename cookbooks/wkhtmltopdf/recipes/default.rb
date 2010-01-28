4e6f2e6081d4c263afb62335d0aa40ec86835ae1
mmastoras <mmastoras@proaptitude.com>
2010-01-28 11:20:50 -0800

    updates

--- cookbooks/wkhtmltopdf/recipes/default.rb
+++ cookbooks/wkhtmltopdf/recipes/default.rb
@@ -5,11 +5,11 @@
 if ['solo', 'app', 'app_master'].include?(node[:instance_role])
  
   # be sure to replace "app_name" with the name of your application.
-  run_for_app('main_site_staging', 'coney_island') do |app_name, data|
+  run_for_app('main_site', 'coney_island') do |app_name, data|
     
     bash "install-qt-webkit" do
       user "root"
-      code "sudo chmod sudo echo 'x11-libs/qt-webkit ~amd64 ~x86' >> /etc/portage/package.keywords/ec2;sudo apt-get libqt4-webkit"
+      code "sudo -s;echo 'dev-haskell/x11 ~amd42 ~x86' >> /etc/portage/package.keywords/ec2;echo 'x11-libs/qt-gui ~amd42 ~x86' >> /etc/portage/package.keywords/ec2;echo 'x11-libs/qt-script ~amd42 ~x86' >> /etc/portage/package.keywords/ec2;echo 'x11-libs/qt-core ~amd42 ~x86' >> /etc/portage/package.keywords/ec2;echo 'x11-libs/qt3support ~amd42 ~x86' >> /etc/portage/package.keywords/ec2;echo 'x11-libs/qt-sql ~amd42 ~x86' >> /etc/portage/package.keywords/ec2;echo 'x11-libs/qt-webkit ~amd42 ~x86' >> /etc/portage/package.keywords/ec2;emerge x11;emerge x11-libs/qt-webkit;exit"
     end
   end
  
