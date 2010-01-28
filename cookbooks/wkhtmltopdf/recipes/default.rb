8843b03f2960888d2186f4a68ef171ad32f1ee02
mmastoras <mmastoras@proaptitude.com>
2010-01-27 19:26:44 -0800

    wkhtmltopdf updates

--- cookbooks/wkhtmltopdf/recipes/default.rb
+++ cookbooks/wkhtmltopdf/recipes/default.rb
@@ -7,9 +7,14 @@ if ['solo', 'app', 'app_master'].include?(node[:instance_role])
   # be sure to replace "app_name" with the name of your application.
   run_for_app('main_site_staging', 'coney_island') do |app_name, data|
     
-    bash "install-pkg-dependencies" do
+    bash "unmask-qt-webkit" do
       user "root"
-      code "sudo echo 'x11-libs/qt-webkit ~amd64 ~x86' >> /etc/portage/package.keywords/ec2;sudo apt-get libqt4-webkit"
+      code "sudo echo 'x11-libs/qt-webkit ~amd64 ~x86' >> /etc/portage/package.keywords/ec2"
+    end
+
+    bash "install-qt-webkit" do
+      user "root"
+      code "sudo apt-get libqt4-webkit"
     end
   end
  
