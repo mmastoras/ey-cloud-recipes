cd4f6993875a2a1b0940dc74d48b9d798a182e50
mmastoras <mmastoras@proaptitude.com>
2010-04-09 15:49:33 -0700

    updates

--- cookbooks/ci_crontab/recipes/default.rb
+++ cookbooks/ci_crontab/recipes/default.rb
@@ -2,8 +2,15 @@
 # Cookbook Name:: ci_crontab
 # Recipe:: default
 #
+remote_file "/tmp/pac.crontab" do
+  owner pac
+  group pac
+  mode 0755
+  source "crontab"
+  backup false
+  action :create
+end
+
 execute "setup-ci-crontab" do
-  command "(echo '15 * * * * cd /data/coney_island/current;rake RAILS_ENV=production scheduled_tasks:hourly >> /data/coney_island/current/log/hourly.log 2>&1';) | uniq | crontab -u #{node[:owner_name]} -"
-  command "(echo ''30 11 * * 6 cd /data/coney_island/current;rake RAILS_ENV=production scheduled_tasks:weekly >> /data/coney_island/current/log/weekly.log 2>&1';) | uniq | crontab -u #{node[:owner_name]} -"
-  command "(echo '0 12 * * * cd /data/coney_island/current;rake RAILS_ENV=production scheduled_tasks:daily >> /data/coney_island/current/log/daily.log 2>&1') | uniq | crontab -u #{node[:owner_name]} -"
+  command "(cat '/tmp/pac.crontab' | uniq | crontab -u #{node[:owner_name]} -"
 end
