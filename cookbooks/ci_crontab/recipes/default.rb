572beaa5cd3ce660ded8d400510ac020c2dd9174
mmastoras <mmastoras@proaptitude.com>
2010-04-10 13:21:27 -0700

    updates

--- cookbooks/ci_crontab/recipes/default.rb
+++ cookbooks/ci_crontab/recipes/default.rb
@@ -3,23 +3,25 @@
 # Recipe:: default
 #
 
-remote_file "/tmp/pac.crontab" do
-  owner 'pac'
-  group 'pac'
-  mode 0755
-  if ['ci_util'].include?(node[:instance_role])
-    source "crontab.util"
-  elsif ['solo'].include?(node[:instance_role])
-    source "crontab.solo"
-  elsif ['app', 'app_master'].include?(node[:instance_role])
-    source "crontab.app"
+if ['solo', 'app', 'app_master'].include?(node[:instance_role])
+  remote_file "/tmp/pac.crontab" do
+    owner 'pac'
+    group 'pac'
+    mode 0755
+    if ['ci_util'].include?(node[:instance_role])
+      source "crontab.util"
+    elsif ['solo'].include?(node[:instance_role])
+      source "crontab.solo"
+    elsif ['app', 'app_master'].include?(node[:instance_role])
+      source "crontab.app"
+    end
+    backup false
+    action :create
   end
-  backup false
-  action :create
-end
 
-execute "setup-crontab" do
-  command "cat '/tmp/pac.crontab' | sed 's/<environment>/#{node[:environment][:framework_env]}/' | crontab -u #{node[:owner_name]} -"
+  execute "setup-crontab" do
+    command "cat '/tmp/pac.crontab' | sed 's/<environment>/#{node[:environment][:framework_env]}/' | crontab -u #{node[:owner_name]} -"
+  end
 end
 
 
